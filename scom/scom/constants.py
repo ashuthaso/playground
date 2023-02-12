e58bb8fe2c6bf773e74c4e8ba8ede61d09bff3ec
Tosh <git@chaoticenginerd.com>
2023-02-12 16:27:50 +0545

    Fix incorrect parsing of config value

--- scom/scom/constants.py
+++ scom/scom/constants.py
@@ -3,6 +3,6 @@ import configparser
 parser = configparser.ConfigParser()
 parser.read("scom.conf")
 
-HEADER_SIZE = parser.get("scom_config", "header_size")
-PORT = parser.get("scom_config", "port")
+HEADER_SIZE = int(parser.get("scom_config", "header_size"))
+PORT = int(parser.get("scom_config", "port"))
 DISCONNECT_MSG = parser.get("scom_config", "disconnect_msg")
