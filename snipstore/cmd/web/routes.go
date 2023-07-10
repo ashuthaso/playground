0e4cf20a8de002c17465517f2bd1e95eb6e9e56b
Tosh <git@chaoticenginerd.com>
2023-07-10 19:09:02 +0545

    snipstore: isolate app routes

---
+++ snipstore/cmd/web/routes.go
@@ -0,0 +1,16 @@
+package main
+
+import "net/http"
+
+func (app *application) routes() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", app.index)
+	mux.HandleFunc("/snippet", app.showSnippet)
+	mux.HandleFunc("/snippet/create", app.createSnippet)
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	return mux
+}
