9e945905d134f35989deb050b7e8127577aa8cf9
Tosh <git@chaoticenginerd.com>
2023-07-10 16:49:55 +0545

    snipstore: add distinct logging for info and errors

--- snipstore/cmd/web/main.go
+++ snipstore/cmd/web/main.go
@@ -4,23 +4,43 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
+
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
 
 func main() {
 	addr := flag.String("addr", "4000", "HTTP network address")
 	flag.Parse()
 
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime|log.LUTC)
+
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
+
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", index)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
+	mux.HandleFunc("/", app.index)
+	mux.HandleFunc("/snippet", app.showSnippet)
+	mux.HandleFunc("/snippet/create", app.createSnippet)
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Printf("Starting server on port %s", *addr)
-	err := http.ListenAndServe(":"+(*addr), mux)
-	log.Fatal(err)
+	srv := &http.Server{
+		Addr:     ":" + *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
+	infoLog.Printf("Starting server on port %s", srv.Addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
